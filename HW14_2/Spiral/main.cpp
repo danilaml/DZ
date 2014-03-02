7c2b0e2e4e530b03dc6e74c1b8e14df1a9d8e702
Danila Malyutin <flashmozzg@gmail.com>
2014-03-02 18:30:49 +0400

    Now you can manually enter matrix and chose class

--- HW14_2/Spiral/main.cpp
+++ HW14_2/Spiral/main.cpp
@@ -10,17 +10,47 @@ int main()
 //    ar[0] = new int[3] {1, 2, 3};
 //    ar[1] = new int[3] {4, 5, 6};
 //    ar[2] = new int[3] {7, 8, 9};
-    int** ar = new int*[4];
-    ar[0] = new int[4] {1, 2, 3, 4};
-    ar[1] = new int[4] {5, 6, 7, 8};
-    ar[2] = new int[4] {9, 10, 11, 12};
-    ar[3] = new int[4] {13, 14, 15, 16};
-    Displayer* dispc = new DispConsole();
-    Displayer* dispf = new DispFile();
+//    int** ar = new int*[4];
+//    ar[0] = new int[4] {1, 2, 3, 4};
+//    ar[1] = new int[4] {5, 6, 7, 8};
+//    ar[2] = new int[4] {9, 10, 11, 12};
+//    ar[3] = new int[4] {13, 14, 15, 16};
 
-    dispc->display(ar, 4);
-    dispf->display(ar, 4);
+    int n = 0;
+    cout << "Enter number of columns: ";
+    cin >> n;
+    int** ar = new int*[n];
+    for(int i = 0; i < n; i++)
+    {
+        cout << "Enter column #" << i << endl;
+        int* tmp = new int[n];
+        for(int j = 0; j < n; j++)
+        {
+            cin >> tmp[j];
+        }
+        ar[i] = tmp;
+    }
 
+    cout << "Enter 'f' to use DispFile or 'c' to use DispConsole" << endl;
+    char c;
+    cin >> c;
+    switch (c)
+    {
+    case 'f':
+    {
+        Displayer* dispf = new DispFile();
+        dispf->display(ar, n);
+        break;
+    }
+    case 'c':
+    {
+        Displayer* dispc = new DispConsole();
+        dispc->display(ar, n);
+        break;
+    }
+    default:
+        cout << "Incorrect parameters";
+    }
     return 0;
 }
 
