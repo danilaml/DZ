0828fbaced992ef6949349f02a2c013dfeed9475
Danila Malyutin <flashmozzg@gmail.com>
2014-03-04 18:45:34 +0400

    small style change in list interface

--- HW14_1/List/mylist.h
+++ HW14_1/List/mylist.h
@@ -3,12 +3,12 @@
 
 class MyList {
 public:
+    virtual ~MyList() {}
     virtual bool insert(int el, int pos) = 0;
     virtual void printList() = 0;
     virtual int length() = 0;
     virtual int getElementAt(int n) = 0;
     virtual bool deleteElementAt(int n) = 0;
-    virtual ~MyList() {}
 };
 
 #endif // MYLIST_H
