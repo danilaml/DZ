8739e1aa5281da93e3ad0e6ec48fc5b68dc744c4
Danila Malyutin <flashmozzg@gmail.com>
2015-02-15 19:38:08 +0300

    Haskell first homework

---
+++ 244/HW15_1/FactorialAndFibonacci/factandfib.hs
@@ -0,0 +1,14 @@
+factorial n = if (n < 0) then error "Factorial doesn't take negative numbers" else
+              if (n == 0)
+              then 1
+              else n * factorial (n-1)
+			  
+fibonacci n = if (n < 0) then error "Fibonacci doesn't take negative numbers" else
+              if (n == 1 || n ==0)
+              then 1
+              else fibonacci (n-1) + fibonacci (n-2)
+
+main = putStrLn (
+                "factorial of 5 is: " ++ (show (factorial 5)) ++ "\n" ++
+                "6th Fibonacci number is: " ++ (show (fibonacci 6))
+                )
