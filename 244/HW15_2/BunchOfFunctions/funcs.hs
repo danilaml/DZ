33a00073e72d9d6e37118eb9f5d3efb0f03542ec
Danila Malyutin <d.maljutin@yandex.ru>
2015-02-21 18:59:52 +0300

    Second Haskell homework

---
+++ 244/HW15_2/BunchOfFunctions/funcs.hs
@@ -0,0 +1,22 @@
+reverse' :: [a] -> [a]
+reverse' list = let helper (x : xs) res = helper xs (x : res)
+                    helper [] res = res
+                in helper list []
+                
+powers :: Integer -> [Integer]
+powers n = [2^p | p <- [0..n]]
+
+digitSum :: Integer -> Integer
+digitSum num = if (num /= 0)
+               then (num `mod` 10 + digitSum (num `div` 10))
+               else 0
+
+find :: (Eq a, Num a) => a -> [a] -> Maybe Integer
+find num list = let find' (x : xs) pos = if (x == num)
+                                        then Just pos
+                                        else find' xs (pos + 1)
+                    find' [] pos = Nothing
+                in find' list 0
+
+isPolindrom :: [Char] -> Bool
+isPolindrom str = str == reverse str
