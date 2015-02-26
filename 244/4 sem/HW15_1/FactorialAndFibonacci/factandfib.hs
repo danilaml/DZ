8b80eea329aa8be8554d2b54e63d8d4a0fa30d3c
Danila Malyutin <flashmozzg@gmail.com>
2015-02-26 19:11:18 +0300

    should look a little better

--- 244/4 sem/HW15_1/FactorialAndFibonacci/factandfib.hs
+++ 244/4 sem/HW15_1/FactorialAndFibonacci/factandfib.hs
@@ -2,13 +2,13 @@ factorial :: Integer -> Integer
 factorial n = if (n < 0) then error "Factorial doesn't take negative numbers" else
               if (n == 0)
               then 1
-              else n * factorial (n-1)
+              else n * factorial (n - 1)
 
 fibonacci :: Integer -> Integer			  
 fibonacci n = if (n < 0) then error "Fibonacci doesn't take negative numbers" else
-              if (n == 1 || n ==0)
+              if (n == 1 || n == 0)
               then 1
-              else fibonacci (n-1) + fibonacci (n-2)
+              else fibonacci (n - 1) + fibonacci (n - 2)
 
 main = putStrLn (
                 "factorial of 5 is: " ++ (show (factorial 5)) ++ "\n" ++
