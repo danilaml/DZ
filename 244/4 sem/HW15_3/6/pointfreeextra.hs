9f087096a6dbb4b6cad8474c67081292d0797388
Danila Malyutin <flashmozzg@gmail.com>
2015-04-24 18:38:05 +0300

    HW3.6 simplified a little

--- 244/4 sem/HW15_3/6/pointfreeextra.hs
+++ 244/4 sem/HW15_3/6/pointfreeextra.hs
@@ -5,7 +5,9 @@ func f g = (filter f) . (map g)
 
 func f = ((filter f) .) . (map) == (.) ((.) (filter f)) (map)
  == (flip (.)) (map) ((.) (filter f))
-func = (.) ((flip (.)) (map)) ((.) . filter)
+
+func = (.) ((flip (.)) (map)) ((.) . filter) == (.) (. map) ((.) . filter)
+ == (. map) . (.) . filter
 -}
 
-func f g l = (.) ((flip (.)) (map)) ((.) . filter)
+func = (. map) . (.) . filter
