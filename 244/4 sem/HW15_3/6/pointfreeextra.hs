71a9aa53c65c6d46f5696a4804fd8c924d486e05
Danila Malyutin <flashmozzg@gmail.com>
2015-04-24 17:45:09 +0300

    HW3.6 fixed

--- 244/4 sem/HW15_3/6/pointfreeextra.hs
+++ 244/4 sem/HW15_3/6/pointfreeextra.hs
@@ -1,9 +1,11 @@
-{--
+{-
 func f g l = filter f (map g l)
 
-func f g l = filter (f.g) l
+func f g = (filter f) . (map g)
 
-func f g = filter (f.g) == filter ((.) f g)
---}
+func f = ((filter f) .) . (map) == (.) ((.) (filter f)) (map)
+ == (flip (.)) (map) ((.) (filter f))
+func = (.) ((flip (.)) (map)) ((.) . filter)
+-}
 
-func = (filter .) . (.)
+func f g l = (.) ((flip (.)) (map)) ((.) . filter)
